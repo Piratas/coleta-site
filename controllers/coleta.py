089f538af269b4f00f2878b5085efee219191412
Iuri Guilherme <iuri@riseup.net>
2015-05-30 10:33:53 -0300

    Copiando esqueleto do 'welcome' e definindo escopo

---
+++ controllers/coleta.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+	redirect(URL('coleta'))
+
+def coleta():
+	"""
+	example action using the internationalization operator T and flash
+	rendered by views/default/index.html or views/generic.html
+
+	if you need a simple wiki simply replace the two lines below with:
+	return auth.wiki()
+	"""
+	response.flash = T("Hello World")
+#	item = db.item(request.args(0,cast=int)) or redirect(URL('default', 'index'))
+	itens = [
+		dict(
+			id = item['id'],
+			conteudo = item['conteudo'],
+		)
+		for item in db().select(db.item.ALL)
+	]
+	return dict(message=T('Coleta de assinaturas para Piratas'),itens=itens)
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
